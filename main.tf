910ef5c46861809c0c86b3c894c766a7b961d9fe
Daniel Reardon <danielreardon@google.com>
2025-07-13 12:55:04 -0400

    Update to include requirement for my Organization

--- main.tf
+++ main.tf
@@ -2,5 +2,6 @@ resource "google_storage_bucket" "example-bucket" {
   name          = "${var.PROJECT_ID}-example"
   location      = "US"
   force_destroy = true
+  uniform_bucket_level_access = true
   project = var.PROJECT_ID
 }
